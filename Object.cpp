c9c363ce95beaa88e647b4893878165d518facdf
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-09-27 16:22:03 +0900

    추상 클래스 Object 기본틀 작성

---
+++ Object.cpp
@@ -0,0 +1,16 @@
+﻿#include "Object.h"
+#include "Mesh.h"
+
+Object::Object() {
+	m_mesh = new Mesh{ };
+	m_mesh->Init();
+}
+
+Object::~Object() {
+	if (m_mesh) delete m_mesh;
+	m_mesh = nullptr;
+}
+
+void Object::Render() {
+	m_mesh->Render();
+}
---
+++ Object.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+// 실질적인 물체를 의미하는 Object클래스
+class Object {
+public:
+	Object();
+	~Object();
+
+	struct DIR {
+		int x{ };
+		int y{ };
+	};
+
+	enum DIR_NUM {
+		LEFT,
+		LEFT_UP,
+		UP,
+		RIGHT_UP,
+		RIGHT,
+		RIGHT_DOWN,
+		DOWN,
+		LEFT_DOWN,
+		DIR_NULL,
+	};
+
+	DIR m_dirSet[9]{
+		{ -1, 0 },
+		{ -1, 1 },
+		{ 0, 1 },
+		{ 1, 1 },
+		{ 1, 0 },
+		{ 1, -1 },
+		{ 0, -1 },
+		{ -1, -1 },
+		{ 0, 0 }
+	};
+
+protected:
+	// 중심점 x, y
+	float x{ };
+	float y{ };
+
+	// 위아래 크기
+	float sizeX{ };
+	float sizeY{ };
+
+	// 회전 각도
+	float m_rotateAngle{ };
+
+	// Mesh객체
+	class Mesh* m_mesh{ nullptr };
+
+public:
+	virtual void Update() = 0;
+	virtual void Render();
+};
+
